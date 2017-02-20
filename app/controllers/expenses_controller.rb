0dcab3796846fc57fd25d489e489195d548a08ff
Jara2392 <daniel.jaramillog@udea.edu.co>
2017-02-20 16:51:09 -0500

    search issue fixed

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,10 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+     #@expenses = Expense.order("date DESC")
+     if params[:concept]
+       @expenses = Expense.where('concept LIKE ? AND category_id LIKE ?', "%#{params[:concept].downcase}", "%#{params[:category_id]}")
+     else
+       @expenses = Expense.all
+     end
   end
 end
