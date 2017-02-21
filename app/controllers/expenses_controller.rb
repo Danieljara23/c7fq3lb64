f098093a41ed3a97f876b91a07e9bb0b3fadeae3
Jara2392 <daniel.jaramillog@udea.edu.co>
2017-02-20 20:05:03 -0500

    devise is already working and index only show user expenses. Also, if user is not signed in or logged in, he can't see theri expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,10 +1,12 @@
 class ExpensesController < ApplicationController
+  before_action :authenticate_user!, only: :index
+
   def index
      #@expenses = Expense.order("date DESC")
      if params[:concept]
-       @expenses = Expense.where('concept LIKE ? AND category_id LIKE ?', "%#{params[:concept].downcase}", "%#{params[:category_id]}")
+       @expenses = current_user.expenses.where('concept LIKE ? AND category_id LIKE ?', "%#{params[:concept].downcase}", "%#{params[:category_id]}")
      else
-       @expenses = Expense.all
+       @expenses = current_user.expenses.all
      end
   end
 end
